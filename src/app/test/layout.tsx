80e29ea7e75ad5bed6439cd03affd52317f24826
isurunvn <isurunaveen27@gmail.com>
2025-06-21 00:37:20 +0530

    added the layout and side bar for common pages

---
+++ src/app/test/layout.tsx
@@ -0,0 +1,11 @@
+import Sidebar from '../../components/SideBar'
+import React, { ReactNode } from 'react';
+
+export default function TestLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 p-6 bg-gray-50">{children}</main>
+    </div>
+  );
+}
