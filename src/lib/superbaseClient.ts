908a28637fe46b7a0126be456dc864ac8b9c2351
Nadun Madusanka <nadunmadusanka564@gmail.com>
2025-07-19 01:42:37 +0530

    fix bugs in login feture problem

--- src/lib/superbaseClient.ts
+++ src/lib/superbaseClient.ts
@@ -1,22 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-supabase.auth.onAuthStateChange(async (event, session) => {
-  if (event === 'SIGNED_IN' && session) {
-    try {
-        const user_role = localStorage.getItem("user_role")
-      // Only update user role if it's not already set
-      if (!session.user.user_metadata?.user_role) {
-        await supabase.auth.updateUser({
-          data: { user_role }
-        })
-      }
-    } catch (error) {
-      console.error('Error updating user role:', error)
-    }
-  }
-})
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
